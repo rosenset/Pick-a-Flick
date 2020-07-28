b0eba1b3202d319391c6f4bba04e1bf5ff31892a
armst209 <armst209@gmail.com>
2020-07-27 20:41:07 -0400

    merging branch

--- src/app/movieapp.service.ts
+++ src/app/movieapp.service.ts
@@ -1,9 +1,50 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs'
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieappService {
 
-  constructor() { }
+  movieGenre : any;
+
+  constructor(private http: HttpClient) { }
+
+  getGenre(): Observable<any>{
+
+    const parameters = {
+
+    api_key: '6a5e8e2794d9936ea3a65f37b3b40410',
+
+    }
+    return this.http.get('https://api.themoviedb.org/3/genre/movie/list', {params: parameters})
+  }
+  
+  getMovies(searchCrit: any){
+
+    console.log("getMovies got called");
+    console.log(searchCrit)
+    const parameters = {
+
+      api_key: '6a5e8e2794d9936ea3a65f37b3b40410',
+      with_genres: searchCrit.genreId
+
+    }
+
+    return this.http.get('https://api.themoviedb.org/3/discover/movie', {params: parameters})
+  }
+
+  getReleaseYear(searchCrit: any){
+    console.log("getRY got called");
+    console.log(searchCrit);
+    const parameters = {
+      
+      api_key: '6a5e8e2794d9936ea3a65f37b3b40410',
+      primary_release_year: searchCrit.releaseYear
+
+    }
+    return this.http.get('https://api.themoviedb.org/3/discover/movie', {params: parameters})
+
+  }
 }
--- src/app/search-criteria/search-criteria.component.ts
+++ src/app/search-criteria/search-criteria.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MovieappService } from '../movieapp.service'
 
 @Component({
   selector: 'app-search-criteria',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchCriteriaComponent implements OnInit {
 
-  constructor() { }
+  genres : any;
 
-  ngOnInit(): void {
+
+  constructor(private movieAppService: MovieappService) { }
+
+  ngOnInit(): any {
+
+    
+    this.movieAppService.getGenre().subscribe(this.onSuccess.bind(this), this.onError.bind(this))
   }
 
+  onSuccess(data:any){
+    this.genres = data.genres;
+    console.log(data.genres);
+  }
+
+  onError(error: Error){
+    console.log(error.message);
+  }
+  searchSubmit(data){
+    console.log("searchSubmit method got called")
+    console.log(data);
+    this.movieAppService.getMovies(data).subscribe(response => console.log(response))
+  }
 }
